40809a38e0e8db4eb621613fafef54bbc57ed534
wikananda <nandasantana276@gmail.com>
2026-03-03 21:46:58 +0800

    edited projects data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -17,29 +17,29 @@ export const projects: Project[] = [
     },
     {
         title: "Dynamic Image Classification",
-        description: "Dynamic image classification code for any datasets.",
-        image: "/sona-ai.png",
+        description: "Image classification code for any datasets.",
+        image: "/image-classification.png",
         github: "https://github.com/wikananda/image-classification-for-dynamic-datasets",
         tags: ["Image Classification", "CNN", "Transformer", "PyTorch"]
     },
     {
         title: "Woman's E-commerce Clothing Review Topic Modeling",
         description: "Topic modeling with LDA and BERTopic and clustering using K-Means and HDBSCAN",
-        image: "/sona-ai.png",
+        image: "/woman-topic-modeling.png",
         github: "https://github.com/wikananda/Womens-E-Commerce-Clothing-Review-Topic-Modeling",
         tags: ["Topic Modeling", "NLP", "Clustering", "Data Analysis"]
     },
     {
         title: "Store Sales Forecasting - Ensemble Gradient Boosting",
         description: "Kaggle competition for store sales forecasting",
-        image: "/sona-ai.png",
+        image: "/store-sales.png",
         github: "https://github.com/wikananda/store-sales-forecasting",
         tags: ["Data Science", "Time Series", "Gradient Boosting", "Kaggle Comp."]
     },
     {
         title: "Partial Makeup Transfer by Template using SSAT++",
         description: "Partial makeup transfer by template implementing SSAT++",
-        image: "/sona-ai.png",
+        image: "/makeup-ssat.png",
         github: "https://github.com/wikananda/Partial-makeup-transfer-by-template",
         tags: ["GAN", "FastAPI", "Makeup Transfer", "PyTorch"]
     },
